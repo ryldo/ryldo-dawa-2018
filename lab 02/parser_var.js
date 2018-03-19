ecc167d5f5d8cf85206008515f6621ff647cd021
YR\HP <ryldo.quispe@tecsup.edu.pe>
2018-03-19 15:04:57 -0500

    commit lab 02

---
+++ lab 02/parser_var.js
@@ -0,0 +1,26 @@
+function parse_vars(req){
+
+	var arreglo_parametros = [],
+	parametros = [],
+	valores = [],
+
+	if (req.url.indexOf('?')>0 ){
+		var url_data = req.url.split ('?');
+		var arreglo_parametros = url_data[1].split('&');	
+
+	}
+
+	for (var i=0; i<arreglo_parametros.length; i++){
+		var parametro = arreglo_parametros[i];
+		var param_data = parametro.split('=');
+		parametros[i]=param_data[0];
+		valores[i] = param_data[1];
+
+	}
+	return{
+		parametros:parametros,
+		valores: valores
+	}
+}
+
+module.exports.parse_vars=parse_vars;
---
+++ lab 02/variables.js
@@ -0,0 +1,29 @@
+var http = require('http');
+    fs	  = require('fs');
+var  parametros = [];
+valores = [];
+arreglo_parametros = [];
+
+http.createServer(function(req,res){
+	fs.readFile('./form.html', function(err,html){
+		var html_string = html.toString();
+
+		if (req.url.indexOf('?')>0) {
+			var url_data = req.url.split('?');
+			arreglo_parametros = url_data[1].split('&');
+		}
+		for (var i = 0; i < arreglo_parametros.length; i++) {
+			var parametro = arreglo_parametros[i];
+			var param_data = parametro.split('=');
+			parametros[i] = param_data[0];
+			valores[i] = param_data[1];
+	}
+	for (var i = 0; i < parametros.length; i++) {
+		html_string = html_string.replace('{'+parametros[i]+'}', valores[i]);
+	}
+		res.writeHead(200,{'Content-type':'text/html'});
+		res.write(html_string);
+		res.end();
+
+	});
+}).listen(3000);
