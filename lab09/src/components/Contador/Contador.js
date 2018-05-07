fa4d46d9fa33412131350ca412561ec9b772be7b
ryldo <ryldo.quispe@tecsup.edu.pe>
2018-05-07 15:10:08 -0500

    laboratorio terminado

--- lab09/src/components/Contador/Contador.js
+++ lab09/src/components/Contador/Contador.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+import  classes from './Contador.css';
 
 export default class Contador extends Component{
+    state = {
+        cont:this.props.valor
+    }
+    disminuirHandler=()=> {
+        this.setState ({
+            cont:this.state.cont - 1
+        });
+    }
+    aumentarHandler = ()=> {
+        this.setState({
+            cont: this.state.cont + 1
+        });
+         }
+    resetHandler = () => {
+            this.setState({
+                cont: this.props.valor
+            });
+        }
+    
     render(){
         let advertencia = (<p> iniciamos con un numero a 5! </p>);
         if (this.props.valor<=5) advertencia = null;
-        return (<div>
+        return (<div className={classes.Contador}>
             <h1>este es mi componete contador</h1>
             <p>este contador iniciara en: {this.props.valor}</p>
             {advertencia}
+            <p>Numero actual:{this.state.cont}</p>
+            <button onClick={this.disminuirHandler}>Disminuir</button>
+            <button onClick={this.resetHandler}>Resetear</button>
+            <button onClick={this.aumentarHandler}>Aumentar</button>
         </div>)
     }   }
